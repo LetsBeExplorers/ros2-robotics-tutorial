48900dffb667ceea15a5bee9b9e76973f49e4d67
raytracer <ray3618@gmail.com>
2026-03-01 22:19:21 -0700

    added node class for obstacle avoidance

--- ros2_ws/src/perception/perception/obstacle_avoidance.py
+++ ros2_ws/src/perception/perception/obstacle_avoidance.py
@@ -1,33 +1,50 @@
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 import math
 
-def scan_callback(self, msg):
 
-    left_min = float('inf')
-    front_min = float('inf')
-    right_min = float('inf')
+class ObstacleAvoidance(Node):
 
-    for i, distance in enumerate(msg.ranges):
-        if math.isinf(distance) or math.isnan(distance):
-            continue
+    def __init__(self):
+        super().__init__('obstacle_avoidance')
 
-        angle = msg.angle_min + i * msg.angle_increment
+        # Create subscriber to /scan
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.scan_callback,
+            10
+        )
 
-        # Front sector: -30° to +30°
-        if -math.radians(30) <= angle <= math.radians(30):
-            front_min = min(front_min, distance)
+    def scan_callback(self, msg):
 
-        # Left sector: 30° to 90°
-        elif math.radians(30) < angle <= math.radians(90):
-            left_min = min(left_min, distance)
+        left_min = float('inf')
+        front_min = float('inf')
+        right_min = float('inf')
 
-        # Right sector: -90° to -30°
-        elif -math.radians(90) <= angle < -math.radians(30):
-            right_min = min(right_min, distance)
+        for i, distance in enumerate(msg.ranges):
+            if math.isinf(distance) or math.isnan(distance):
+                continue
 
-    self.get_logger().info(
-        f"L: {left_min:.2f} F: {front_min:.2f} R: {right_min:.2f}"
-    )
+            angle = msg.angle_min + i * msg.angle_increment
+
+            # Front sector: -30° to +30°
+            if -math.radians(30) <= angle <= math.radians(30):
+                front_min = min(front_min, distance)
+
+            # Left sector: 30° to 90°
+            elif math.radians(30) < angle <= math.radians(90):
+                left_min = min(left_min, distance)
+
+            # Right sector: -90° to -30°
+            elif -math.radians(90) <= angle < -math.radians(30):
+                right_min = min(right_min, distance)
+
+        self.get_logger().info(
+            f"L: {left_min:.2f} F: {front_min:.2f} R: {right_min:.2f}"
+        )
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -35,6 +52,7 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()

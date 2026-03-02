639b3d74e40b8b9f8bda096b508b1855d62f6415
raytracer <ray3618@gmail.com>
2026-03-01 17:49:08 -0700

    Remove ROS 2 build/install/log artifacts from repository

--- ros2_ws/build/perception/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/raytracer/projects/DASE4460/ros2-uav-demo/ros2_ws/install/perception'
